3b58b57d71e5811f92193ece600898232f7f1f7b
Yannic Nuwenhof <ynuwenhof@pm.me>
2021-12-19 03:07:27 +0100

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+pub struct Opts {
+    /// Set listener address
+    #[clap(short, long, default_value_t = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))]
+    addr: IpAddr,
+    /// Set listener port
+    #[clap(short, long, default_value_t = 1080)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _opts = Opts::parse();
 }
